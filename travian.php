d66b67afa2840d640d3bf2a3872cabf0c068706a
Pedro Valero <pedro.valero@estudiante.uam.es>
2015-08-16 11:29:48 +0200

    Codigo sucio que envia el heroe a la aventura

--- travian.php
+++ travian.php
@@ -4,8 +4,8 @@
 	//set POST variables
 	$url = 'http://ts5.travian.net/dorf1.php';
 	$fields = array(
-                    'name' => urlencode('Filtor'),
-                    'password' => urlencode('47230660Ee'),
+                    'name' => urlencode('Digimon'),
+                    'password' => urlencode('noirerve'),
                     'lowRes' => urlencode('1'),
                     'w' => urlencode(''),
                     'login' => urlencode('1437574738')
@@ -54,76 +54,123 @@
 //////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 // Code to show available adventures
-    $relativeUrl = 'hero_adventure.php';
-	$adventureList = array();
-
-	print "Initializing hero's adventure data\n";
-
-    curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $relativeUrl);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($ch);
-
-    $doc = new DomDocument();
-    $doc->loadHTML($result);
-    $adventures = $doc->getElementById('adventureListForm')->getElementsByTagName('tr');
-
-    // The first element will be the title of the table so we have to skip it
-    $firstElement = true;
-    foreach ($adventures as $i => $adv) {
-        if($firstElement){
-            $firstElement = false;
-        } else {
-        	$fields = array();
-
-        	// Add the link needed to go to this adventure
-            $fields['href'] = $adv->getElementsByTagName('a')->item(1)->getAttribute('href');
-
-            // Getting hidden fields
-            foreach ($adv->getElementsByTagName('input') as $j => $input){
-                $fields[$input->getAttribute('name')]=$input->getAttribute('value');
-            }
-
-            $adventureList[trim($adv->getElementsByTagName('td')->item(2)->childNodes->item(0)->nodeValue) . ' <=> ' . $adv->getElementsByTagName('td')->item(4)->childNodes->item(1)->nodeValue] = $fields;
-        }
-    }
-
-    $counter = 0;
-    foreach($adventureList as $time => $adv){
-        echo "Aventura ". $counter++ . " encontrada a: " . $time;
-        //foreach($adv as $index => $data){
-        //    echo "[".$index."]=".$data;
-        //} 
-        print "\n";
-    } 
+//    $relativeUrl = 'hero_adventure.php';
+//	$adventureList = array();
+//
+//	print "Initializing hero's adventure data\n";
+//
+//    curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $relativeUrl);
+//    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+//    $result = curl_exec($ch);
+//
+//    $doc = new DomDocument();
+//    $doc->loadHTML($result);
+//    $adventures = $doc->getElementById('adventureListForm')->getElementsByTagName('tr');
+//
+//    // The first element will be the title of the table so we have to skip it
+//    $firstElement = true;
+//    foreach ($adventures as $i => $adv) {
+//        if($firstElement){
+//            $firstElement = false;
+//        } else {
+//        	$fields = array();
+//
+//        	// Add the link needed to go to this adventure
+//            $fields['href'] = $adv->getElementsByTagName('a')->item(1)->getAttribute('href');
+//
+//            // Getting hidden fields
+//            foreach ($adv->getElementsByTagName('input') as $j => $input){
+//                $fields[$input->getAttribute('name')]=$input->getAttribute('value');
+//            }
+//
+//            $adventureList[trim($adv->getElementsByTagName('td')->item(2)->childNodes->item(0)->nodeValue) . ' <=> ' . $adv->getElementsByTagName('td')->item(4)->childNodes->item(1)->nodeValue] = $fields;
+//        }
+//    }
+//
+//    $counter = 0;
+//    foreach($adventureList as $time => $adv){
+//        echo "Aventura ". $counter++ . " encontrada a: " . $time;
+//        //foreach($adv as $index => $data){
+//        //    echo "[".$index."]=".$data;
+//        //}
+//        print "\n";
+//    }
 //////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 // Code to start an adventure
-    $position = 0;
+//    $position = 0;
+//
+//    echo $adventureList[array_keys($adventureList)[$position]]['href'];
+//    echo "\n";
+//	curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $adventureList[array_keys($adventureList)[$position]]['href']);
+//    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+//
+//    // Acces to the $position adventure
+//    $result = curl_exec($ch);
+//
+//    //print $result;
+//    $doc = new DomDocument();
+//    $doc->loadHTML($result);
+//
+//    // Get the form that we have to submit to send the hero to the adventure
+//    $action = $doc->getElementsByTagName('form')[0]->getAttribute('action');
+//
+//    // Get the inputs of the form
+//    $inputs = $doc->getElementsByTagName('input');
+//    $fields = array();
+//
+//    foreach($inputs as $i => $data){
+//        $fields[$data->getAttribute('name')]=$data->getAttribute('value');
+//    }
+////    $fields_string = '';
+////    for ($x = 0; $x < count($adventureList); $x++) {
+////    	$firstElement = true;
+////		foreach($adventureList[array_keys($adventureList)[$x]] as $key=>$value) {
+////			if($firstElement){
+////				$firstElement = False;
+////			}else{
+////				$fields_string .= $key.'='.urlencode($value).'&';
+////				echo $key . "  " . $value . " \n";
+////			}
+////		}
+////	}
+////	$fields_string = rtrim($fields_string, '&');
+//    $fields_string = '';
+//    foreach($fields as $key=>$value) {
+//        $fields_string .= $key.'='.urlencode($value).'&';
+//    }
+//    $fields_string = rtrim($fields_string, '&');
+//
+//    curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $action);
+//    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+//	curl_setopt($ch,CURLOPT_POST, count($fields));
+//	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+//
+//    $result = curl_exec($ch);
+//    //print $result;
+//	//close connection
+//	curl_close($ch);
 
-    echo $adventureList[array_keys($adventureList)[$position]]['href'];
-	curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $adventureList[array_keys($adventureList)[$position]]['href']);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
-    $fields_string = '';
-    for ($x = 0; $x <= 15; $x++) {
-    	$firstElement = true;
-		foreach($adventureList[array_keys($adventureList)[$x]] as $key=>$value) {
-			if($firstElement){
-				$firstElement = False;
-			}else{
-				$fields_string .= $key.'='.urlencode($value).'&';
-				echo $key . "  " . $value . " \n";
-			}
-		}
-	}
-	$fields_string = rtrim($fields_string, '&');
-
-	curl_setopt($ch,CURLOPT_POST, count($fields));
-	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-    $result = curl_exec($ch);    
-    //print $result;
-	//close connection
-	curl_close($ch);
-
+//////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+// Code to send attack
+//    $relativeUrl = "build.php?id=39&tt=2"; // Access to "plaza de torneos" an move to "enviar tropas"
+//    curl_setopt($ch,CURLOPT_URL, 'http://ts5.travian.net/' . $relativeUrl);
+//    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+//    $result = curl_exec($ch);
+//
+//    $doc = new DomDocument();
+//    $doc->loadHTML($result);
+//
+//    $action = $doc->getElementsByTagName('form')[0]->getAttribute('action');
+//
+//    $inputs = $doc->getElementsByTagName('input');
+//
+//    foreach($inputs as $index => $data){
+//        echo $data->getAttribute('name');
+//        echo "\n";
+//    }
+//    echo $inputs->length;
+//    echo "\n";
 ?>
